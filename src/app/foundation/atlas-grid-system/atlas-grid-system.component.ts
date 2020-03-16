63643f2d512137f4d63c71c2bd7244d0aa8e7d81
Afftene Taylor <aftaylor@jacksonandcoker.com>
2020-03-16 14:17:10 -0400

    Added Media Queries in Grid System Page

--- src/app/foundation/atlas-grid-system/atlas-grid-system.component.ts
+++ src/app/foundation/atlas-grid-system/atlas-grid-system.component.ts
@@ -9,23 +9,31 @@ export class AtlasGridSystemComponent implements OnInit {
   deviceRanges = [
     {
       icon: "fas fa-mobile-alt fa-7x",
-      rangeNumber: "From 0 to 576px"
+      rangeNumber: "From 0 to 576px",
+      viewportOnly: "$atlas-mobile-xs-range"
     },
     {
       icon: "fas fa-mobile-alt fa-rotate-270 fa-7x",
-      rangeNumber: "From 576px to 768px"
+      rangeNumber: "From 576px to 768px",
+      viewportOnly: "$atlas-mobile",
+      rangeOnly: "$atlas-mobile-range"
     },
     {
       icon: "fas fa-tablet-alt fa-7x",
-      rangeNumber: "From 768px to 991px"
+      rangeNumber: "From 768px to 991px",
+      viewportOnly: "$atlas-tablet",
+      rangeOnly: "$atlas-tablet-range"
     },
     {
       icon: "fas fa-laptop fa-7x",
-      rangeNumber: "From 992px to 1200px"
+      rangeNumber: "From 992px to 1200px",
+      viewportOnly: "$atlas-desktop",
+      rangeOnly: "$atlas-desktop-range"
     },
     {
       icon: "fas fa-desktop fa-7x",
-      rangeNumber: "From 1200px to greater"
+      rangeNumber: "From 1200px to greater",
+      viewportOnly: "$atlas-desktop-lg"
     }
   ]
 
