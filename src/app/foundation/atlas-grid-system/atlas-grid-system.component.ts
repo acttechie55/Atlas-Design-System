a76ccf804cc4975f8557744c1f6d00c88fa018f3
Afftene Taylor <aftaylor@jacksonandcoker.com>
2020-04-02 12:15:14 -0400

    Change Breakpoint Variable Names

--- src/app/foundation/atlas-grid-system/atlas-grid-system.component.ts
+++ src/app/foundation/atlas-grid-system/atlas-grid-system.component.ts
@@ -10,25 +10,25 @@ export class AtlasGridSystemComponent implements OnInit {
     {
       icon: "fas fa-mobile-alt fa-7x",
       rangeNumber: "From 0 to 576px",
-      viewportOnly: "$atlas-mobile-xs-range"
+      viewportOnly: "$atlas-mobile-xs-max"
     },
     {
       icon: "fas fa-mobile-alt fa-rotate-270 fa-7x",
       rangeNumber: "From 576px to 768px",
       viewportOnly: "$atlas-mobile",
-      rangeOnly: "$atlas-mobile-range"
+      rangeOnly: "$atlas-mobile-max"
     },
     {
       icon: "fas fa-tablet-alt fa-7x",
       rangeNumber: "From 768px to 991px",
       viewportOnly: "$atlas-tablet",
-      rangeOnly: "$atlas-tablet-range"
+      rangeOnly: "$atlas-tablet-max"
     },
     {
       icon: "fas fa-laptop fa-7x",
       rangeNumber: "From 992px to 1200px",
       viewportOnly: "$atlas-desktop",
-      rangeOnly: "$atlas-desktop-range"
+      rangeOnly: "$atlas-desktop-max"
     },
     {
       icon: "fas fa-desktop fa-7x",
