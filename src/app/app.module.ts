bf7246f376fc98ce9b945b45751a457c70621870
Afftene Taylor <aftaylor@jacksonandcoker.com>
2020-04-02 15:40:11 -0400

    change z index variables, created keyframes scss file,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import 'bootstrap';
+// import 'bootstrap';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'; 
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
