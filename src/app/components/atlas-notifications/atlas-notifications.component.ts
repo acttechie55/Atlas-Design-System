234a048f6f27eccaa76c347af032ae2a7b563a30
Afftene Taylor <aftaylor@jacksonandcoker.com>
2020-05-15 16:37:43 -0400

    add prism again, add json

--- src/app/components/atlas-notifications/atlas-notifications.component.ts
+++ src/app/components/atlas-notifications/atlas-notifications.component.ts
@@ -1,14 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
+declare var ClipboardJS:any;
 @Component({
   selector: 'app-atlas-notifications',
   templateUrl: './atlas-notifications.component.html'
 })
 export class AtlasNotificationsComponent implements OnInit {
 
-  constructor() { }
+  navigationalSelect:string ="info";
+  public checked: boolean = false;
+  showCode:boolean = true;
+  clipboardTargetID:string = "code-info";
 
-  ngOnInit() {
+  test:boolean = false;
+  products: any = [];
+
+  constructor(private httpClient: HttpClient){}
+  ngOnInit(){
+    new ClipboardJS('#btnCopy');
+    this.httpClient.get("./assets/data/code/notifications.json").subscribe(data =>{
+      console.log(data);
+      this.products = data;
+    })
   }
+ 
+
+  navigationalSelectChange(){
+    switch(this.navigationalSelect) { 
+      case "info": { 
+         //statements; 
+         this.clipboardTargetID = 'code-info';
+         break; 
+      } 
+      case "danger": { 
+        //statements; 
+        this.clipboardTargetID = 'code-danger';
+        break; 
+      }
+      case "caution": { 
+      //statements; 
+        this.clipboardTargetID = 'code-caution';
+        break; 
+      } 
+      case "success": { 
+        this.clipboardTargetID = 'code-success';
+         break; 
+      } 
+      default: { 
+         //statements; 
+         this.clipboardTargetID = 'code-info';
+         break; 
+      } 
+   } 
+  }
+  
 
 }
